dee613c5d95c8c071e5a507dc26c2a758aa61da2
Andrey68 <alimov.andrey68@gmail.com>
2017-03-14 14:22:21 +0200

    master: add request sms permission for marshmallow and above android versions

--- app/src/main/java/com/dev/kt/ktsmsreceiver/MainActivity.java
+++ app/src/main/java/com/dev/kt/ktsmsreceiver/MainActivity.java
@@ -1,22 +1,30 @@
 package com.dev.kt.ktsmsreceiver;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.dev.kt.ktsmsreceiver.utils.DeviceManager;
 import com.dev.kt.ktsmsreceiver.utils.PermissionHelper;
 import com.dev.kt.ktsmsreceiver.utils.SmsService;
 import com.dev.kt.ktsmsreceiver.utils.StoragePref;
+import com.tbruyelle.rxpermissions.RxPermissions;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.container_permission)
+    View mViewPermission;
+
+    @BindView(R.id.container_main)
+    View mViewMain;
 
     @BindView(R.id.text_view_status)
     TextView mTextViewStatus;
@@ -27,45 +35,59 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.button_enable)
     Button mButtonEnable;
 
+    @OnClick(R.id.button_disable)
+    void onClickButtonDisable() {
+        stopService(new Intent(MainActivity.this, SmsService.class));
+        changeViewToEnableService();
+    }
+
+    @OnClick(R.id.button_enable)
+    void onClickButtonEnable() {
+        Intent mServiceIntent = new Intent(MainActivity.this, SmsService.class);
+        startService(mServiceIntent);
+
+        changeViewToDisableService();
+    }
+
+    @OnClick(R.id.button_allow_permission)
+    void onClickPermission() {
+        askPermission();
+    }
+
+    private RxPermissions mRxPermissions;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        if (!PermissionHelper.hasSMSPermission(this)) {
-            // ask for permission
-            Toast.makeText(this, "Version is upper Lollipop, SMS permission is not available", Toast.LENGTH_LONG).show();
-        } else {
+        mRxPermissions = new RxPermissions(this);
 
+        if (!PermissionHelper.hasSMSPermission(this)) {
+            askPermission();
+        } else {
             if (DeviceManager.isServiceRunning(this, SmsService.class)) {
                 changeViewToDisableService();
             } else {
                 changeViewToEnableService();
             }
-
-            mButtonDisable.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    stopService(new Intent(MainActivity.this, SmsService.class));
-
-                    changeViewToEnableService();
-                }
-            });
-
-            mButtonEnable.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    Intent mServiceIntent = new Intent(MainActivity.this, SmsService.class);
-                    startService(mServiceIntent);
-
-                    changeViewToDisableService();
-                }
-            });
         }
 
+    }
+
+    private void askPermission() {
+        mRxPermissions
+                .request(Manifest.permission.RECEIVE_SMS)
+                .subscribe(granted -> {
+                    if (granted) {
+                        mViewPermission.setVisibility(View.GONE);
+                        mViewMain.setVisibility(View.VISIBLE);
+                    } else {
+                        mViewPermission.setVisibility(View.VISIBLE);
+                        mViewMain.setVisibility(View.GONE);
+                    }
+                });
     }
 
     // better use some kind of MVP architecture but project too small
